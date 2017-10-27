980de0309515984eaa84333384740cc7c71d8d60
Vincent van Beek <vincentvanbeek@home.nl>
2017-10-27 10:39:47 +0200

    Added BirthDate to datamodel

--- MVCBasics/Models/Profile.cs
+++ MVCBasics/Models/Profile.cs
@@ -17,7 +17,8 @@ namespace MVCBasics.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Gender { get; set; }
+        public bool Gender { get; set; }
+        public System.DateTime BirthDate { get; set; }
     
         public virtual Address Address { get; set; }
     }
